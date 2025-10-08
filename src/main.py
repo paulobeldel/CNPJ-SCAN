4a5d8eb672bda930649e2d8e1cf7bda7ce59a878
Martha Lacerda <martinha.lacerda@gmail.com>
2025-10-07 21:32:33 -0300

    config: dotenv

--- src/main.py
+++ src/main.py
@@ -7,10 +7,24 @@ Responsável por
 """
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers import cnpj, reports
+from config import settings # Importa as configuracoes
+
+# Define o titulo da API usando a variavel
+app = FastAPI(title=f"Minha API - {settings.API_VERSION}")
+
+# 1. Configurando o CORS (MUITO importante para o React)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.list_cors_origins, # Permite que o frontend se conecte
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Instancia principal da aplicação
-app = FastAPI()
+# app = FastAPI()
 
 # Inclui o roteador na aplicação
 app.include_router(cnpj.router)
